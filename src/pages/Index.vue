500eb1b528277e4392ae922b6e6cc4bb62e3fba5
David Hawkins <david@sipheren.com>
2020-04-12 16:52:01 +1000

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,7 @@
     <div class="row  justify-center items-center content-center">
       <h3>Welcome</h3>
     </div>
-    <div class="row  justify-center items-center content-center">
+    <div class="row  justify-center items-center content-center" style="text-align:center;">
       <p>For information on our series, entry forms and communication please head to our Facebook Group, click the logo below.</p>
     </div>
     <div class="row  justify-center items-center content-center" style="text-align:center;">
