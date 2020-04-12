8671bfb42c86eba948ed8e2cf60afc5c5199d1cf
David Hawkins <david@sipheren.com>
2020-04-12 14:28:37 +1000

    fb events

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -4,7 +4,7 @@
       <h3>Events</h3>
     </div>
     <div class="full-width row  justify-center items-start content-center">
-
+      <div class="fb-group" data-href="https://www.facebook.com/groups/632070486958150/" data-width="" data-show-social-context="true" data-show-metadata="false"></div>
     </div>
   </q-page>
 </template>
@@ -14,7 +14,7 @@
   height: calc(99vh - 90px);
 }
 </style>
-
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v6.0&appId=227505741941405&autoLogAppEvents=1"></script>
 <script>
 export default {
   name: 'PageIndex'
